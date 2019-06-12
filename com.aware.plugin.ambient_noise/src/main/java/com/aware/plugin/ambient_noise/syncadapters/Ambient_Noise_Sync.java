af8224dce1303dad94d0a29032d9b65bc87426cf
Denzil Ferreira <ferreiradenzil@gmail.com>
2019-06-12 12:24:10 +0300

    - Updated libraries, documentation in GitHub.

--- com.aware.plugin.ambient_noise/src/main/java/com/aware/plugin/ambient_noise/syncadapters/Ambient_Noise_Sync.java
+++ com.aware.plugin.ambient_noise/src/main/java/com/aware/plugin/ambient_noise/syncadapters/Ambient_Noise_Sync.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.IBinder;
 
-import androidx.annotation.Nullable;
 import com.aware.plugin.ambient_noise.Provider;
 import com.aware.syncadapters.AwareSyncAdapter;
 
@@ -34,7 +33,6 @@ public class Ambient_Noise_Sync extends Service {
         }
     }
 
-    @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return sSyncAdapter.getSyncAdapterBinder();
